6c3ec6088a03ea79df337b56d30a198493f99bad
Miryam <miryamo95@kprschools.ca>
2022-11-10 23:40:16 -0500

    Status 11:40 pm

--- TShapes.py
+++ TShapes.py
@@ -31,13 +31,58 @@ class Shape3D:
         return None
 
 
-class Octahedron(Shape3D):
-    def __init__(self, type, colour, edge, x, y, z):
+class Octapoints:
+    def __init__(self, A1=(0, 0, -1), A2=(-1, 0, 0), A3=(0, 0, 1), A4=(1, 0, 0), B1=(0, 1, 0), C1=(0, -1, 0)):
+        self.A1 = A1
+        self.A2 = A2
+        self.A3 = A3
+        self.A4 = A4
+        self.B1 = B1
+        self.C1 = C1
+
+    def set_A1(self, A1):
+        self.A1 = A1
+
+    def set_A2(self, new):
+        self.A2 = (new)
+
+    def set_A3(self, new):
+        self.A3 = (new)
+
+    def set_A4(self, new):
+        self.A4 = (new)
+
+    def set_B1(self, new):
+        self.B1 = (new)
+
+    def set_C1(self, new):
+        self.C1 = (new)
+
+    def get_coordinates(self):
+        print(
+            f'Point 1: {self.A1}\nPoint 2: {self.A2}\nPoint 3: {self.A3}\nPoint 4: {self.A4}\nPoint 5: {self.B1}\nPoint 6: {self.C1}')
+
+
+class Hexapoints:
+    def __init__(self, O=(0, 8, 0), A=(-5, 0, 0), B=(-2.5, 0, 4.33), C=(2.5, 0, 4.33), D=(5, 0, 0), E=(2.5, 0, -4.33),
+                 F=(-2.5, 0, -4.33)):
+        self.O = O
+        self.A = A
+        self.B = B
+        self.C = C
+        self.D = D
+        self.E = E
+        self.F = F
+
+
+class Octahedron(Shape3D, Octapoints):
+    def __init__(self, type, colour, edge, A1=(0, 0, -1), A2=(-1, 0, 0), A3=(0, 0, 1), A4=(1, 0, 0), B1=(0, 1, 0),
+                 C1=(0, -1, 0)):
         super().__init__(type, colour)
+        super(Shape3D, self).__init__()
         self.edge = edge
-        self.x = x
-        self.y = y
-        self.z = z
+
+        # Just upload defintion and like the variables there and the methods would be iherited as well. so setting new values would not be hard, adding an additional value would not be bad.
 
     def get_edge(self):
         return self.edge
@@ -56,8 +101,3 @@ class Octahedron(Shape3D):
     def draw(self):
         output = ascii_magic.from_image_file('octa1.png', columns=100, char='@', back=ascii_magic.Back.WHITE)
         ascii_magic.to_terminal(output)
-
-
-class Octapoints:
-    def __init__(self, A1=(0, 0, 1), A2=(0, 1, 0), A3=(1, 0, 0)):
-        pass
